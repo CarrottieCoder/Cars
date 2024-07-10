56443dff7eaaf19e6cea8cfdf884c7540ca17f87
Coder <basia.bugi.coding@icloud.com>
2024-07-10 21:12:41 +0200

    Handle admin

--- c/app/admin.py
+++ c/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import CarSeries, CarMake, Manufacturer
 # Register your models here.
+
+admin.site.register(CarMake)
+admin.site.register(CarSeries)
+admin.site.register(Manufacturer)
