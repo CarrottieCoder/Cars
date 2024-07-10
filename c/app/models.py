c12162069d2d92ef60ce664381d6adea2d3ef47f
Coder <basia.bugi.coding@icloud.com>
2024-07-10 21:26:27 +0200

    Create new fields

--- c/app/models.py
+++ c/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Manufacturer(models.Model):
@@ -24,12 +25,23 @@ class CarSeries(models.Model):
         verbose_name_plural = 'Car Series'
 
 class CarMake(models.Model):
-    series = models.ForeignKey(CarSeries, on_delete=models.CASCADE, related_name="makes")
-    year_manufactured = models.PositiveIntegerField(verbose_name='years_manufactued', 
-        validators=[MinValueValidator(1900), MaxValueValidator(2024)])
+    series = models.ForeignKey(CarSeries, on_delete=models.CASCADE, related_name="makes", default=None)
+    year_manufactured = models.PositiveIntegerField(verbose_name='year_manufactued', 
+        validators=[MinValueValidator(1900), MaxValueValidator(2024)], default=2023)
     mileage = models.PositiveIntegerField()
     price = models.FloatField(validators=[MinValueValidator(0.0)],)
-    # User will go here 
+    engine = models.CharField(max_length=1000, choices=(
+        ("GAS", 'Gas'),
+        ("DIESEL", 'Diesel'),
+        ('HYBRID', 'Hybrid'),
+        ('ELECTRIC', 'Electric'),
+        ('Hydrogen', 'Hydrogen'),
+        ('OTHER', 'Other')
+    ), default="Gas")
+    accident_free = models.BooleanField(default=True)
+    origin_country = models.CharField(max_length=1000, default="USA")
+    other_data = models.TextField(default="")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cars", default=1)
 
     def __str__(self):
         return f"{self.series.__str__()} {self.year_manufactured} \n Mileage: {self.mileage} \n Price: {self.price}$"
