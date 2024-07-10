1e9c7bc91b593ab46c98f364927101c9a4132f30
Coder <basia.bugi.coding@icloud.com>
2024-07-10 21:08:50 +0200

    Create models

--- c/app/models.py
+++ c/app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100)
+    origin = models.CharField(max_length=100)
+    origin_demonym = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} from {self.origin}"
+
+class CarSeries(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="series")
+    name = models.CharField(max_length=100)
+    car_class = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.manufacturer.name} {self.name}" 
+
+class CarMake(models.Model):
+    series = models.ForeignKey(CarSeries, on_delete=models.CASCADE, related_name="makes")
+    year_manufactured = models.PositiveIntegerField(verbose_name='years_manufactued', 
+        validators=[MinValueValidator(1900), MaxValueValidator(2024)])
+    mileage = models.PositiveIntegerField()
+    price = models.FloatField(validators=[MinValueValidator(0.0)],)
+    # User will go here 
+
+    def __str__(self):
+        return f"{self.series.__str__()} {self.year_manufactured} \n Mileage: {self.mileage} \n Price: {self.price}$"
