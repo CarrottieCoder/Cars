ae9486c0e872db800b90a2249a887fa003bfb394
Coder <basia.bugi.coding@icloud.com>
2024-07-11 13:35:33 +0200

    Create a view for CarMake [retrieve]

--- c/app/views.py
+++ c/app/views.py
@@ -10,9 +10,9 @@ def index(request):
 
 def car(request, pk):
     try: 
-        car = Car.objects.get(pk=pk)
-    except Car.DoesNotExist:
+        car = CarMake.objects.get(pk=pk)
+    except CarMake.DoesNotExist:
         raise Http404("Car does not exist")
-    return render(request, template_name="car.html", content={
+    return render(request, template_name="car.html", context    ={
         "car": car,
     })
