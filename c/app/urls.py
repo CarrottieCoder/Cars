691fb6d9ef1a85afa2173b47597b45f892c3b673
Coder <basia.bugi.coding@icloud.com>
2024-07-11 13:34:20 +0200

    Add URL for retrieve Car object

--- c/app/urls.py
+++ c/app/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import *
 urlpatterns = [
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('cars/<int:pk>', car, name="car")
 ]
