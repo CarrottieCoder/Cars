25e3555a402a701f3f061733435ef3476e097ca4
Coder <basia.bugi.coding@icloud.com>
2024-07-11 13:33:03 +0200

    Create a view for a single car

--- c/app/views.py
+++ c/app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from .models import *
+from django.http import Http404
 # Create your views here.
 def index(request):
     cars = CarMake.objects.all()
     return render(request, template_name='index.html', context={
         "cars": cars
+    })
+
+def car(request, pk):
+    try: 
+        car = Car.objects.get(pk=pk)
+    except Car.DoesNotExist:
+        raise Http404("Car does not exist")
+    return render(request, template_name="car.html", content={
+        "car": car,
     })
