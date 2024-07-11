50142bd512ae4743e508be2a6329252e97187c66
Coder <basia.bugi.coding@icloud.com>
2024-07-11 14:05:36 +0200

    Create filter

--- c/app/views.py
+++ c/app/views.py
@@ -3,9 +3,22 @@ from .models import *
 from django.http import Http404
 # Create your views here.
 def index(request):
-    cars = CarMake.objects.all()
+    # Enable filters
+    if request.GET and "f" in request.GET:
+        origin_country = request.GET.get('origin_country')
+        accident_free = request.GET.get('accident_free')
+        engine = request.GET.get('engine')
+        cars = CarMake.objects.filter(
+            origin_country=origin_country,
+            accident_free=accident_free,
+            engine=engine,
+            )
+        
+    else:
+        cars = CarMake.objects.all()
+
     return render(request, template_name='index.html', context={
-        "cars": cars
+        "cars": cars,
     })
 
 def car(request, pk):
