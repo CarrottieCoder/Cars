0f917a5cdf00b039fc9bdee047c91c08c8f3545d
Coder <basia.bugi.coding@icloud.com>
2024-07-11 13:24:48 +0200

    Create formatted_price field. Fix floatfield

--- c/app/models.py
+++ c/app/models.py
@@ -29,7 +29,7 @@ class CarMake(models.Model):
     year_manufactured = models.PositiveIntegerField(verbose_name='year_manufactued', 
         validators=[MinValueValidator(1900), MaxValueValidator(2024)], default=2023)
     mileage = models.PositiveIntegerField()
-    price = models.FloatField(validators=[MinValueValidator(0.0)],)
+    price = models.PositiveIntegerField(validators=[MinValueValidator(0.0)],)
     engine = models.CharField(max_length=1000, choices=(
         ("GAS", 'Gas'),
         ("DIESEL", 'Diesel'),
@@ -46,5 +46,15 @@ class CarMake(models.Model):
     def __str__(self):
         return f"{self.year_manufactured} {self.series.__str__()} ({self.id})"
     
+    def formatted_price(self):
+        x = str(self.price)
+        n = ""
+        for i in range(len(x)):
+            if i % 3 == 0:
+                n += " "
+            n += x[i]
+
+        return n
+
     def mileage_to_kilometers(self):
         return round(self.mileage * 1.60934)
