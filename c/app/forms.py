cf73b70d3b96355148f4c4e211fe87bde8cf6c03
Coder <basia.bugi.coding@icloud.com>
2024-07-12 12:52:50 +0200

    Create creation form for CarMake

--- c/app/forms.py
+++ c/app/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core import validators
+from .models import CarSeries
 
 class RegisterForm(forms.Form):
     username = forms.CharField(validators=[
@@ -10,3 +11,32 @@ class RegisterForm(forms.Form):
     
     class Meta:
         fields = ['username', 'password']
+
+class CreateCarForm(forms.Form):
+    series = forms.ModelChoiceField(queryset=CarSeries.objects.all(), required=True)
+    year_manufactued = forms.IntegerField(validators=[
+        validators.MinValueValidator(1900, "That's not a year"),
+        validators.MaxValueValidator(2024, "The car can't be newer than 2024")
+    ], required=True)
+    mileage = forms.IntegerField(validators=[
+        validators.MinValueValidator(0)
+    ], required=True)
+    price = forms.IntegerField(validators=[
+        validators.MinValueValidator(0)
+    ], required=True)
+    engine = forms.ChoiceField(choices=(
+        ("GAS", 'Gas'),
+        ("DIESEL", 'Diesel'),
+        ('HYBRID', 'Hybrid'),
+        ('ELECTRIC', 'Electric'),
+        ('Hydrogen', 'Hydrogen'),
+        ('OTHER', 'Other')
+    ), required=True)
+    accident_free = forms.BooleanField(required=True)
+    origin_country = forms.CharField(validators=[
+        validators.MinLengthValidator(3)
+    ], required=True)
+    other_data = forms.Textarea()
+    
+
+    
